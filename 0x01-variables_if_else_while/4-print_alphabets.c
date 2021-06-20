8d59bb44993a5bd4102ce3ddff65d3f429c3ab11
hazzoum <hazthe12@gmail.com>
2021-06-20 15:01:16 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print Alpha
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int j;
+for (j = 97 ; j < 123 ; j++)
+{
+if (j == 101 || j == 113)
+{
+j++;
+}
+putchar ((char)j);
+}
+putchar ('\n');
+return (0);
+}
