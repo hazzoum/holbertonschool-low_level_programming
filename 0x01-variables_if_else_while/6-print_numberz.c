91477774cbbf619fb0b7e4fcbe67e1177ee3c190
hazzoum <hazthe12@gmail.com>
2021-06-21 05:41:52 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print Numberz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int j;
+for (j = 48 ; j < 58 ; j++)
+{
+putchar ((char)j);
+}
+putchar ('\n');
+return (0);
+}
