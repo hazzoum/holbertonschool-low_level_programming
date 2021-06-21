fe165941ff828e6214d2a7a0a3a29a6fdb33a0bb
hazzoum <hazthe12@gmail.com>
2021-06-21 05:43:55 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print Numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int j;
+for (j = 122 ; j > 96 ; j--)
+{
+putchar ((char)j);
+}
+putchar ('\n');
+return (0);
+}
