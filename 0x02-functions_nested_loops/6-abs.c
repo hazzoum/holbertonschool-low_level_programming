688926711351285ca8e5766cc750046a3f198825
hazzoum <hazthe12@gmail.com>
2021-06-22 14:09:22 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _abs - print the value absolute.
+ * @n: The integer .
+ *
+ * Return: the value absolute of integer.
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
