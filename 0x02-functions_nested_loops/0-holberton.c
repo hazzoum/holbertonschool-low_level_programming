df189ce25a433c6e59ec00419e7dcdae86a654ac
hazzoum <hazthe12@gmail.com>
2021-06-22 14:25:44 -0700

    task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
-	/**
- 	* main - Task 0
- 	*
- 	* Return: Always 0 (Success)
- 	*/
-		int main(void)
-		{
-		char ch[9] = "Holberton";
-		int i;
-			for (i = 0; i < 9; i++)
-			{		
-			_putchar (ch[i]);
-			}
-			_putchar ('\n');
-			return (0);
-		}
+/**
+* main - Task 0
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch[9] = "Holberton";
+int i;
+for (i = 0; i < 9; i++)
+{		
+_putchar (ch[i]);
+}
+_putchar ('\n');
+return (0);
+}
