408610f2e4e6141dbb80115fa70625200da97fef
hazzoum <hazthe12@gmail.com>
2021-06-28 10:00:32 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _puts - print string.
+* @str: the input.
+* Return: length.
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
+}
