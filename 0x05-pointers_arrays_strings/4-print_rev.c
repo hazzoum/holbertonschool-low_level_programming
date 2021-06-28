3b72a446dcb1fa52ed8c35b0552f8d4be541bc47
hazzoum <hazthe12@gmail.com>
2021-06-28 10:01:55 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* print_rev - print string.
+* @s: the input.
+*/
+
+void print_rev(char *s)
+{
+int i = strlen(s) - 1;
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
