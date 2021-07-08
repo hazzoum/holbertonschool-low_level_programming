0f6c90907e227f61f7f4423340bbc7bfa70ec4f9
hazzoum <hazthe12@gmail.com>
2021-07-08 13:06:40 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* leet- encodes a string .
+* @s: the input.
+* Return: s.
+*/
+char *leet(char *s)
+{
+int i, x, y;
+char letters[] = "oOlLeEaAtT";
+char integers[] = "0011334477";
+for (i = 0; s[i] != '\0'; i++)
+{
+y = 0;
+for (x = 0; letters[x] != '\0'; x++)
+{
+if (s[i] == letters[x])
+{
+y = x;
+s[i] = integers[y];
+}
+}
+y++;
+}
+return (s);
+}
