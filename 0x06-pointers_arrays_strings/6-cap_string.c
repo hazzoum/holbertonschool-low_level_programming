4cd1e9cd12a2832924141c3d6222647125c69d97
hazzoum <hazthe12@gmail.com>
2021-07-08 13:04:45 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* cap_string- capitalizes all words of a string.
+* @x: the input.
+* Return: str .
+*/
+char *cap_string(char *x)
+{
+int i;
+for (i = 0; x[i] != '\0'; i++)
+{
+if ((x[i] == ',') || (x[i] == ';') || (x[i] == '.') || (x[i] == '!')
+|| (x[i] == '?') || (x[i] == '"') || (x[i] == '(') || (x[i] == ')')
+|| (x[i] == '{') || (x[i] == '}') || (x[i] == ' ') || (x[i] == '\t')
+|| (x[i] == '\n'))
+{
+if ((x[i + 1] >= 'a') && (x[i + 1] <= 'z'))
+x[i + 1] = x[i + 1] - 32;
+}
+if ((x[0] >= 'a') && (x[0] <= 'z'))
+x[0] = x[0] - 32;
+}
+return (x);
+}
