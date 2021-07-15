b1f6ae4b784b95412f432f841244abacc9def3cb
hazzoum <hazthe12@gmail.com>
2021-07-15 12:56:41 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,22 +1,41 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+void *set_mem(void *p, unsigned int nmemb, unsigned int size, char ch);
 /**
- **_calloc- allocate memory for an array
- *@nmemb: number of elements
- *@size: size of each element
- *Return: return NULL in two cases and return p
+ * _calloc - allocates memory for an array, and inits
+ *
+ * @nmemb: number of members in array
+ * @size: size of each member
+ *
+ * Return: void pointer to beginning of alloc/init'ed memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int x;
-char *p;
-if (nmemb == 0 || size == 0)
-return (NULL);
-p = malloc(nmemb * size);
-if (p == NULL)
-return (NULL);
-for (x = 0; x < (nmemb * size); x++)
-p[x] = '\0';
-return (p);
+	void *T;
+
+	if (nmemb < 1 || size < 1)
+		return (NULL);
+	T = malloc(nmemb * size);
+	if (T == NULL)
+		return (NULL);
+	T = set_mem(T, nmemb, size, '\0');
+	return (T);
+}
+/**
+ * set_mem - sets memory for void pointer
+ *
+ * @p: pointer to beginning of memory to set
+ * @nmemb: number of members of array
+ * @size: size of elements of array
+ * @ch: character to set memory to
+ *
+ * Return: void pointer to beginning of set memory
+ */
+void *set_mem(void *p, unsigned int nmemb, unsigned int size, char ch)
+{
+	char *cast = p;
+	unsigned int i = 0;
+
+	while (i < nmemb * size)
+		cast[i++] = ch;
+	return (p);
 }
